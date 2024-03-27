b5d007f86186261a69854b112eefd4bbafdaa370
Muchael123 <mainamichael2849@gmail.com>
2024-03-27 19:24:46 +0300

    add allowed hosts

--- email_sender/settings.py
+++ email_sender/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q8zp_38sr-ybmp$7dt+l6_r0f%(bb^_7vqzf=f=9@#8%ms&%w(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
