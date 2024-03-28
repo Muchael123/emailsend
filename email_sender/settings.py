20fa92fddb4ec839e9accf88fc7b203a683bfecf
Muchael123 <mainamichael2849@gmail.com>
2024-03-28 08:34:01 +0300

    enable cors

--- email_sender/settings.py
+++ email_sender/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "corsheaders"
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
